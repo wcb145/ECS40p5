5434ecd5919bcbffe3aa0a595da5aa341f1f5cd9
jdduval <jdduval117@gmail.com>
2015-02-14 21:39:32 -0800

    filled in += operator code

--- list.cpp
+++ list.cpp
@@ -16,6 +16,17 @@ LinkedList::~LinkedList()
 
 LinkedList& LinkedList::operator+= (Directory* data)
 {
+  ListNode *ptr = head, *prev = NULL;
   
+  while (ptr && ptr->data < data)
+  {
+    prev = ptr;
+    ptr = ptr->next;    
+  } // prev == NULL
+  
+  if (!prev)
+    head = new ListNode(data, head);
+  else // prev != NULL
+    prev->next = new ListNode(data, ptr);
 }  // operator+=
 
